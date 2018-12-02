98711d2cccfc74bc40f1f63b46efccf5fec596b4
Guilherme <gdevincenzi@gmail.com>
2018-12-02 01:44:50 -0200

    Add second part of solution

--- 01/solution.clj
+++ 01/solution.clj
@@ -8,3 +8,11 @@
        (map read-string)))
 
 (def answer1 (reduce + data))
+
+(defn find-duplicate [acc data]
+  (let [cur (first data)]
+    (if (contains? acc cur)
+      cur
+      (recur (conj acc cur) (rest data)))))
+
+(def answer2 (find-duplicate #{} (reductions + (cycle data))))
