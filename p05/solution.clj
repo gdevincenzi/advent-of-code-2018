e50d5c36a5c6236f97aedc8e9d0ffa5450f8db26
Guilherme <gdevincenzi@gmail.com>
2018-12-07 11:42:42 -0200

    Add solution to part2 of puzzle05

--- p05/solution.clj
+++ p05/solution.clj
@@ -1,18 +1,38 @@
-(ns p05.solution)
+(ns p05.solution
+  (:require
+   [clojure.string :as str]))
 
-(def input (clojure.string/trim-newline (slurp "./input.txt")))
+(def input (str/trim-newline (slurp "./input.txt")))
 
 (defn reacts? [a b]
   (= (Math/abs (compare a b)) 32))
 
-(defn reduce-polimer [polimer]
+(defn reduce-polymer [polymer]
   (reduce
    (fn [acc cur]
      (if (reacts? (first acc) cur)
        (rest acc)
-       (conj acc cur))) '() polimer))
+       (conj acc cur))) '() polymer))
 
 (def answer1
   (->> input
-       (reduce-polimer)
+       (reduce-polymer)
        (count)))
+
+(defn regbuilder [type]
+  (re-pattern (str type "|" (str/upper-case type))))
+
+(def polymer-types (map char (range 97 123)))
+
+(defn remove-polymer-type [polymer type]
+  (str/replace polymer (regbuilder type) ""))
+
+(defn possible-polymers [polymer]
+  (map #(remove-polymer-type polymer %) polymer-types))
+
+(def answer2
+  (->> input
+       (possible-polymers)
+       (map reduce-polymer)
+       (map count)
+       (apply min)))
