26dbba43dfdc51c0a9725cf995df5a9085921020
Guilherme <gdevincenzi@gmail.com>
2018-12-15 04:06:12 -0200

    Add part2 of puzzle08

--- p08/solution.clj
+++ p08/solution.clj
@@ -2,7 +2,7 @@
 
 (def input (slurp "./input.txt"))
 
-(def testinput [2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2])
+(def data (map read-string (re-seq #"\d+" input)))
 
 (defn parsenodes
   ([data]
@@ -22,3 +22,27 @@
 
 (defn solve1 [data]
   (reduce + (flatten (parsenodes data))))
+
+(def answer1 (solve1 data))
+
+(defn parsenodesdetail
+  ([data]
+   (let [[c m & rst] data]
+     (if (zero? c)
+       (let [[nodemeta remainder] (split-at m rst)]
+         [{:meta nodemeta :value (reduce + nodemeta)} remainder])
+       (let [[children remain] (parsenodesdetail c rst)
+             [meta remainder] (split-at m remain)
+             nodevalue (reduce + (map #(:value (nth children (dec %) {:value 0})) meta ))]
+         [{:meta meta :children children :value nodevalue} remainder]))))
+  ([n data] (parsenodesdetail n data []))
+  ([n data acc]
+   (if (zero? n)
+     [acc data]
+     (let [[brother remainder] (parsenodesdetail data)]
+       (recur (dec n) remainder (conj acc brother ))))))
+
+(defn solve2 [data]
+  (:value (first (parsenodesdetail data))))
+
+(def answer2 (solve2 data))
