5c7ba38dcc3a1ca4b7aaa2aca38066686ed8d6b1
Guilherme <gdevincenzi@gmail.com>
2018-12-29 02:21:48 -0200

    Refactor solution for part1

--- p11/solution.clj
+++ p11/solution.clj
@@ -2,10 +2,33 @@
 
 (def input (read-string (slurp "./input.txt")))
 
+(def gridsize 300)
+
 (defn powerlevel [x y serial]
-  (let [id (+ x 10)
-        hundreth (quot (* id (+ serial (* y id))) 100)]
-   (- (read-string (str (first (reverse (str hundreth))))) 5)))
+  (let [id (+ x 10)]
+    (-> id
+        (* ,, y)
+        (+ ,, serial)
+        (* ,, id)
+        (quot ,, 100)
+        (mod ,, 10)
+        (- ,, 5))))
 
-(def grid (for [x (range 1 301) y (range 1 301)] (powerlevel x y input)))
+(def grid (for [x (range 1 (inc gridsize)) y (range 1 (inc gridsize))] (powerlevel x y input)))
 
+(def vectorgrid
+  (->> grid
+       (partition gridsize)
+       (map vec)
+       (vec)))
+
+(defn squarepower [x y]
+  (reduce + (for [x (range (dec x) (+ 2 x)) y (range (dec y) (+ 2 y))] (get-in vectorgrid [x y]))))
+
+(def answer1
+  (let [powers (for [x (range 1 (- gridsize 2)) y (range 1 (- gridsize 2))] {[x y] (squarepower x y)})]
+    (->> powers
+         (reduce conj {})
+         (sort-by val >)
+         (first)
+         (key))))
