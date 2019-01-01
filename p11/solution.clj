e7263cfc86ce838a05a8dae985d2d83e46ef8aa7
Guilherme <gdevincenzi@gmail.com>
2019-01-01 12:36:31 -0200

    Refactor part1 and add part2 of p11

--- p11/solution.clj
+++ p11/solution.clj
@@ -4,6 +4,7 @@
 
 (def gridsize 300)
 
+;; PART 1
 (defn powerlevel [x y serial]
   (let [id (+ x 10)]
     (-> id
@@ -14,7 +15,7 @@
         (mod ,, 10)
         (- ,, 5))))
 
-(def grid (for [x (range 1 (inc gridsize)) y (range 1 (inc gridsize))] (powerlevel x y input)))
+(def grid (for [y (range 1 (inc gridsize)) x (range 1 (inc gridsize))] (powerlevel x y input)))
 
 (def vectorgrid
   (->> grid
@@ -23,12 +24,56 @@
        (vec)))
 
 (defn squarepower [x y]
-  (reduce + (for [x (range (dec x) (+ 2 x)) y (range (dec y) (+ 2 y))] (get-in vectorgrid [x y]))))
+  (reduce + (for [y (range (dec y) (+ 2 y)) x (range (dec x) (+ 2 x))] (get-in vectorgrid [y x]))))
 
 (def answer1
-  (let [powers (for [x (range 1 (- gridsize 2)) y (range 1 (- gridsize 2))] {[x y] (squarepower x y)})]
+  (let [powers (for [y (range 1 (- gridsize 2)) x (range 1 (- gridsize 2))] {[x y] (squarepower x y)})]
     (->> powers
          (reduce conj {})
          (sort-by val >)
          (first)
          (key))))
+
+;; PART 2
+(def sum-node
+  (memoize (fn [x y]
+             (cond
+               (= x y 0) (get-in vectorgrid [y x])
+               (< x 0) 0
+               (< y 0) 0
+               :else (-> (get-in vectorgrid [y x])
+                         (+ ,, (sum-node x (dec y)))
+                         (+ ,, (sum-node (dec x) y))
+                         (- ,, (sum-node (dec x) (dec y))))))))
+
+(def summed-area-table
+  (let [areas (for [y (range 0 gridsize) x (range 0 gridsize)] (sum-node x y))]
+    (->> areas
+         (partition gridsize)
+         (map vec)
+         (vec))))
+
+(defn squarepower [x y size]
+  (let [s (dec size)
+        x0 (- x 2)
+        y0 (- y 2)
+        x1 (+ x0 size)
+        y1 (+ y0 size)]
+    (-> (get-in summed-area-table [y1 x1] 0)
+        (+ ,, (get-in summed-area-table [y0 x0] 0))
+        (- ,, (get-in summed-area-table [y1 x0] 0))
+        (- ,, (get-in summed-area-table [y0 x1] 0)))))
+
+(defn power-reduce [[x y size power] [nx ny nsize]]
+  (let [npower (squarepower nx ny nsize)]
+    (if (> npower power)
+      [nx ny nsize npower]
+      [x y size power])))
+
+(def answer2 
+  (let [gs (inc gridsize)
+        squares (for [x (range 1 gs)
+                      y (range 1 gs)
+                      s (range 1 (- gs (max x y)))]
+                  [x y s])]
+    (reduce #(power-reduce %1 %2) [0 0 0 0] squares)))
