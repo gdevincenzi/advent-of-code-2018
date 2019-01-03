b7d28c6d2b25b0e1ab588dc08b852a8af4fdb02b
Guilherme <gdevincenzi@gmail.com>
2019-01-03 00:39:25 -0200

    Remove test function

--- p12/solution.clj
+++ p12/solution.clj
@@ -2,7 +2,41 @@
   (:require [clojure.zip :as zip
              clojuse.string :as s]))
 
-(def testinput
-  (s)"#..#.#..##......###...###")
+(def input (slurp "./input.txt"))
 
-(def z (zip/vector-zip testinput))
+(def initial-state
+  (->> input
+       (s/split-lines)
+       (first)
+       (drop 15)
+       (apply str)))
+
+(defn parse-rule [rule]
+  (let [[k v] (drop 1 (re-find #"([.|#]{5}) => ([.|#]{1})" rule))]
+    {k v}))
+
+(def rules
+  (->> input
+       (s/split-lines)
+       (drop 2)
+       (map parse-rule)
+       (reduce conj {})))
+
+(defn apply-rule [[_ _ c _ _ :as window]]
+  (get rules (apply str window) (str c)))
+
+(defn iterate-growth [state]
+  (let [padded (str "..." state "...")]
+    (->> padded
+         (partition 5 1)
+         (map apply-rule)
+         (apply str))))
+
+(def answer1
+  (let [final-state (nth (iterate iterate-growth initial-state) 20)]
+    (->> final-state
+         (zipmap (range -20 (count final-state)))
+         (filter #(= (second %) \#))
+         (keys)
+         (reduce +))))
+
