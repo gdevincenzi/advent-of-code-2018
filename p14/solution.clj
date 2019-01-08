496d117062597a1e17593e4b0070a4f53f45c7dc
Guilherme <gdevincenzi@gmail.com>
2019-01-08 00:01:07 -0200

    Refactor part1 of p14

--- p14/solution.clj
+++ p14/solution.clj
@@ -2,23 +2,31 @@
 
 (def input 681901)
 
-(def recipes [3 7])
-
 (defn make-new-recipes [r1 r2]
   (map #(read-string (str %)) (str (+ r1 r2))))
 
 (defn create-recipes [data]
-  (if (map? data)
-    (let [{recipes :recipes
-           elf1 :elf1
-           elf2 :elf2} data
-          recipe1 (get recipes elf1)
-          recipe2 (get recipes elf2)
-          new-recipes (make-new-recipes recipe1 recipe2)
-          all-recipes (apply conj recipes new-recipes)
-          total (count all-recipes)]
-      (assoc data
-             :recipes all-recipes
-             :elf1 (mod (+ elf1 (inc recipe1)) total)
-             :elf2 (mod (+ elf2 (inc recipe2)) total)))
-    {:recipes [3 7 1 0] :elf1 0 :elf2 1}))
+  (let [{recipes :recipes
+         elf1 :elf1
+         elf2 :elf2} data
+        recipe1 (get recipes elf1)
+        recipe2 (get recipes elf2)
+        new-recipes (make-new-recipes recipe1 recipe2)
+        all-recipes (apply conj recipes new-recipes)
+        total (count all-recipes)]
+    (assoc data
+           :recipes all-recipes
+           :elf1 (mod (+ elf1 (inc recipe1)) total)
+           :elf2 (mod (+ elf2 (inc recipe2)) total))))
+
+(def answer1
+  (->> input
+       (+ 10)
+       (nth (iterate create-recipes {:recipes [3 7 1 0] :elf1 0 :elf2 1}))
+       (:recipes)
+       (drop input)
+       (take 10)
+       (apply str)
+       (read-string)))
+
+
